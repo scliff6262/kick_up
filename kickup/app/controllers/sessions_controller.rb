28e0945ca736468cdd8c6cd95d615d8b258f9c11
Sean Clifford <sclifford6262@gmail.com>
2018-04-17 21:46:18 +0000

    setup login process without password

--- kickup/app/controllers/sessions_controller.rb
+++ kickup/app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
 
+  def new #login page
+  end 
 
+  def create #creating a session
+    user = User.find_by(email: session_params[:email])
+
+    if user 
+      redirect_to user_path(user)
+    else 
+      render 'sessions/new'
+    end 
+  end 
+
+  def destroy #logout
+  end 
+
+  private
+
+  def session_params
+    params.require(:user).permit(:email, :password)
+  end 
 end
