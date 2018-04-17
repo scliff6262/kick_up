ab49e9ab51e7ef87aded74b6225e77e2e0baf7ef
Sean Clifford <sclifford6262@gmail.com>
2018-04-17 19:44:08 +0000

    added create and show functions in events controller

--- kickup/app/controllers/events_controller.rb
+++ kickup/app/controllers/events_controller.rb
@@ -1,2 +1,26 @@
 class EventsController < ApplicationController
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.organizer_id = params[:user_id]
+    if @event.save
+      redirect_to user_event_path(params[:user_id], @event)
+    else
+      render "events/new"
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :zip_code, :skill_level, :event_type)
+  end
 end
