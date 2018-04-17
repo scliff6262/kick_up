260850b0b0431f8b411170e9ed5761696d62e5a4
Sean Clifford <sclifford6262@gmail.com>
2018-04-17 21:56:53 +0000

    added authenticate to sessions controller

--- kickup/app/controllers/sessions_controller.rb
+++ kickup/app/controllers/sessions_controller.rb
@@ -1,24 +1,24 @@
 class SessionsController < ApplicationController
 
   def new #login page
-  end 
+  end
 
   def create #creating a session
     user = User.find_by(email: session_params[:email])
 
-    if user 
+    if user && user.authenticate(session_params[:password])
       redirect_to user_path(user)
-    else 
+    else
       render 'sessions/new'
     end 
-  end 
+  end
 
   def destroy #logout
-  end 
+  end
 
   private
 
   def session_params
     params.require(:user).permit(:email, :password)
-  end 
+  end
 end
