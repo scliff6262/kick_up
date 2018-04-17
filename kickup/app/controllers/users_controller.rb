adcadff2fd26bc9410b89e88188d37f963e59f7b
Sean Clifford <sclifford6262@gmail.com>
2018-04-17 16:36:25 +0000

    added edit and update to users controller

--- kickup/app/controllers/users_controller.rb
+++ kickup/app/controllers/users_controller.rb
@@ -10,7 +10,25 @@ class UsersController < ApplicationController
       redirect_to user_path(@user)
     else
       render "/users/new"
-    end 
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.assign_attributes(user_params)
+    if @user && @user.save
+      redirect_to user_path(@user)
+    else
+      render "/users/edit"
+    end
   end
 
   private
