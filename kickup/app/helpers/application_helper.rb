7295fde3fde624377a79ef805eb60c9ca31d7323
Sean Clifford <sclifford6262@gmail.com>
2018-04-18 17:58:56 +0000

    moved location_from_zip method to app helper

--- kickup/app/helpers/application_helper.rb
+++ kickup/app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+    def location_from_zip(zip)
+      location_hash = ZipCodes.identify(zip)
+      "#{location_hash[:city]}, #{location_hash[:state_code]}"
+    end
+
 end
