36ba2e6466987e14e33cddad0b993e11051d9529
Sean Clifford <sclifford6262@gmail.com>
2018-04-18 17:38:08 +0000

    event show page shows properly formatted data

--- kickup/app/helpers/events_helper.rb
+++ kickup/app/helpers/events_helper.rb
@@ -1,2 +1,27 @@
 module EventsHelper
+
+  def friendly_date(mmddyyyy)
+    split_date = mmddyyyy.split("/")
+    ddmmyyyy = "#{split_date[1]}/#{split_date[0]}/#{split_date[2]}}"
+    Date.parse(ddmmyyyy).strftime("%A %B %e, %Y ")
+  end
+
+  def location_from_zip(zip)
+    location_hash = ZipCodes.identify(zip)
+    "#{location_hash[:city]}, #{location_hash[:state_code]}"
+  end
+
+  def skill_level_string(skill_level)
+    case skill_level
+    when 1
+      "Beginners"
+    when 2
+      "Some Experienced Players"
+    when 3
+      "Veterans"
+    when 4
+      "Highly Competitive"
+    end
+  end
+
 end
--- kickup/app/models/event.rb
+++ kickup/app/models/event.rb
@@ -10,7 +10,7 @@ class Event < ActiveRecord::Base
 
   validates :organizer_id, presence: true
 
-  validates :time, presence: true
+  validates :event_time, presence: true
 
   validates :date, presence: true
   validates_format_of :date, :with => /\d{2}\/\d{2}\/\d{4}/, :message => "Date must be in the following format: mm/dd/yyyy"
