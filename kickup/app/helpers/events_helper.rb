118cbf06a1c47193307ae0aa7c853f773da2af79
Sean Clifford <sclifford6262@gmail.com>
2018-04-18 18:00:02 +0000

    removed location_from_zip method, and put in app helpers

--- kickup/app/helpers/events_helper.rb
+++ kickup/app/helpers/events_helper.rb
@@ -6,11 +6,6 @@ module EventsHelper
     Date.parse(ddmmyyyy).strftime("%A %B %e, %Y ")
   end
 
-  def location_from_zip(zip)
-    location_hash = ZipCodes.identify(zip)
-    "#{location_hash[:city]}, #{location_hash[:state_code]}"
-  end
-
   def skill_level_string(skill_level)
     case skill_level
     when 1
