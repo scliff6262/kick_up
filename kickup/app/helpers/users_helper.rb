f2f56141efacd405e87154a43ab5a7f747f2f734
Sean Clifford <sclifford6262@gmail.com>
2018-04-18 18:00:36 +0000

    added user_skill_level to users helper

--- kickup/app/helpers/users_helper.rb
+++ kickup/app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+
+  def user_skill_level(skill_level)
+    case skill_level
+    when 1
+      "Beginner"
+    when 2
+      "Some Experience"
+    when 3
+      "Veteran"
+    when 4
+      "Highly Competitive"
+    end
+  end
+
 end
