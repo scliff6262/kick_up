d4d2deb71b70b6c45185f69cc3dda69ca8252285
Sean Clifford <sclifford6262@gmail.com>
2018-04-18 17:36:14 +0000

    changed create logic to allow creation of time string

--- kickup/app/controllers/events_controller.rb
+++ kickup/app/controllers/events_controller.rb
@@ -6,6 +6,7 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
+    @event.event_time = "#{params[:event]["event_time(4i)"]}:#{params[:event]["event_time(5i)"]}"
     @event.organizer_id = params[:user_id]
     if @event.save
       redirect_to user_event_path(params[:user_id], @event)
@@ -21,6 +22,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:title, :zip_code, :skill_level, :date, :address, :time, :event_type)
+    params.require(:event).permit(:title, :zip_code, :skill_level, :date, :address, :event_type)
   end
 end
