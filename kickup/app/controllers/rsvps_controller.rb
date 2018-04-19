ae54e32e1043e29a7a3061f5c07b93ec3af3f965
Sean Clifford <sclifford6262@gmail.com>
2018-04-19 17:27:06 +0000

    setup rsvps#create and rsvps#show

--- kickup/app/controllers/rsvps_controller.rb
+++ kickup/app/controllers/rsvps_controller.rb
@@ -5,7 +5,18 @@ class RsvpsController < ApplicationController
   end
 
   def create
-    raise params.inspect
+    @rsvp = Rsvp.new(comment: rsvp_params[:comment], event_id: rsvp_params[:event_id])
+    @rsvp.user_id = current_user.id
+    if @rsvp.save
+      redirect_to user_rsvp_path(current_user, @rsvp)
+    else
+      render 'rsvp/new'
+    end
+  end
+
+  def show
+    @rsvp = Rsvp.find(params[:id])
+    @user = current_user
   end
 
   def edit
@@ -15,5 +26,11 @@ class RsvpsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def rsvp_params
+    params.permit(:comment, :event_id)
   end
 end
