b53ff7100c2384b35b2aa61c64ffc4f51432368d
scliff6262 <sclifford6262@gmail.com>
2018-06-27 15:23:38 -0400

    click event for view-attendees button working

--- kickup/app/assets/javascripts/events.js
+++ kickup/app/assets/javascripts/events.js
@@ -1,9 +1,22 @@
-$(function(){
-  $("a.attendeesButton").on("click", function(e){
+$(document).ready(function(){
+
+  function User(json){
+    this.name = json.user["first_name"] + ' ' + json.user["last_name"]
+    this.email = json.user["email"]
+
+    this.addToPage = function(){
+      $(".attendees").append("<p><strong>" + this.name + "</strong> - " + this.email + "</p>")
+    }
+  }
+  $("#attendeesButton").on("click", function(e){
     e.preventDefault()
+    $(".attendees").html("<h4>Members Attending</h4>")
     const eventID = $(e.target).data("id")
-    $.get("/events/" + eventID + "/rsvps", function(r){
-      console.log(r)
+    $.getJSON("/events/" + eventID + "/rsvps", function(r){
+      r.forEach(function(user){
+      	thisUser = new User(user)
+      	thisUser.addToPage()
+      })
     })
   })
 })
