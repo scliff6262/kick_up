7faf6bae4990e2608397c3775fddc6b2bd1fa2ec
scliff6262 <sclifford6262@gmail.com>
2018-06-29 15:56:08 -0400

    removed phone number validation (don't need)

--- kickup/app/models/user.rb
+++ kickup/app/models/user.rb
@@ -3,7 +3,6 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :zip_code, presence: true
   validates_format_of :zip_code, with: /\A\d{5}-\d{4}|\A\d{5}\z/
-  validates :phone, presence: true
   validates :level, presence: true
 
   validates :email, presence: true
